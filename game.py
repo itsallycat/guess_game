1b439e35333ab683977ca09ab8e0de7f8cef1587
Resume/Coventry University Cybersecurity Bootcamp <you@example.com>
2022-11-10 10:55:03 -0800

    creating frist version of the game

---
+++ game.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import random
+
+minimum=1
+maximum=20
+
+answer=random.randit(1, 20)
+
+guess=None
+
+while guess !=answer:
+   guess=int(input("enter a guess:"))
+   
+   if guess==answer
+       print("well done")
+   elif guess>answer:
+       print("too hight!")
+   else:
+       print("too hight")       
